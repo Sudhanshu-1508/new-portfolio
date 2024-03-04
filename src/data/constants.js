9534b0f4412cbf039f571a13de1d886c49c4e473
Sudhanshu-1508 <sudhanshukulkarni55@gmail.com>
2024-03-04 21:34:10 +0530

    full and final changes

--- src/data/constants.js
+++ src/data/constants.js
@@ -9,10 +9,9 @@ export const Bio = {
       "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
     github: "https://github.com/sudhanshu-1508",
     resume:
-      "https://drive.google.com/file/d/1ffZrcMcn8UatXGIaautbbqpV7ADNaETA/view?usp=sharing",
-    linkedin: "https://www.linkedin.com/in/rishav-chanda-b89a791b3/",
-    twitter: "https://twitter.com/RishavChanda",
-    insta: "https://www.instagram.com/rishav_chanda/",
+      "https://drive.google.com/file/d/1vCZT17qoevMgcS8t9iOCSERspR_s-Y31/view?usp=sharing",
+    linkedin: "www.linkedin.com/in/sudhanshu-kulkarni-150800sk",
+    insta: "https://www.instagram.com/sudhanshu.k_/",
   };
   
   export const skills = [
